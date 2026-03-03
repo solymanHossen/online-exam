fc698c59388a06e0e19ab99caf81c047470fb864
Md Solyman Hossen <105260985+solymanHossen@users.noreply.github.com>
2026-03-04 00:59:14 +0600

    chore: update React and TypeScript types to latest versions; wrap app in StrictMode

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -3,6 +3,7 @@ import './bootstrap';
 
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
@@ -17,6 +18,10 @@ createInertiaApp({
     setup({ el, App, props }) {
         const root = createRoot(el);
 
-        root.render(<App {...props} />);
+        root.render(
+            <StrictMode>
+                <App {...props} />
+            </StrictMode>,
+        );
     },
 });
